b5f62531610492d722fe02eebc9f92748ad9f3fa
Rodolfo Ruiz <rpruiz@me.com>
2021-09-30 10:51:53 -0500

    Rebuilding and indexing
    After building all the pages, we render the list as HTML links

--- src/main.rs
+++ src/main.rs
@@ -24,3 +24,59 @@ async fn main() -> Result<(), anyhow::Error> {
         }
     });
 }
+
+fn rebuild_site(content_dir: &str, output_dir: &str) -> Result<(), anyhow::Error> {
+    let _ = fs::remove_dir_all(output_dir);
+
+    let markdown_files: Vec<String> = walkdir::WalkDir::new(content_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().display().to_string().ends_with(".md"))
+        .map(|e| e.path().display().to_string())
+        .collect();
+    let mut html_files = Vec::with_capacity(markdown_files.len());
+
+    for file in &markdown_files {
+        let mut html = templates::HEADER.to_owned();
+        let markdown = fs::read_to_string(&file)?;
+        let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
+
+        let mut body = String::new();
+        pulldown_cmark::html::push_html(&mut body, parser);
+
+        html.push_str(templates::render_body(&body).as_str());
+        html.push_str(templates::FOOTER);
+
+        let html_file = file
+            .replace(content_dir, output_dir)
+            .replace(".md", ".html");
+        let folder = Path::new(&html_file).parent().unwrap();
+        let _ = fs::create_dir_all(folder);
+        fs::write(&html_file, html)?;
+
+        html_files.push(html_file);
+    }
+
+    write_index(html_files, output_dir)?;
+    Ok(())
+}
+
+fn write_index(files: Vec<String>, output_dir: &str) -> Result<(), anyhow::Error> {
+    let mut html = templates::HEADER.to_owned();
+    let body = files
+        .into_iter()
+        .map(|file| {
+            let file = file.trim_start_matches(output_dir);
+            let title = file.trim_start_matches("/").trim_end_matches(".html");
+            format!(r#"<a href="{}">{}</a>"#, file, title)
+        })
+        .collect::<Vec<String>>()
+        .join("<br />\n");
+
+    html.push_str(templates::render_body(&body).as_str());
+    html.push_str(templates::FOOTER);
+
+    let index_path = Path::new(&output_dir).join("index.html");
+    fs::write(index_path, html)?;
+    Ok(())
+}
